9a5eccc942cc556ceb933e6c357d19396ca07cd6
Jérôme Hue <57839770+jeromehue@users.noreply.github.com>
2021-12-12 19:20:17 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,4 @@
-#include "rodos/rodos.h" // includes the RODOS API
+#include "rodos.h" // includes the RODOS API
 
 class HelloWorld : public StaticThread<> { // defines a Thread named HelloWorld
     void run() { // implements the code of the main task of the thread
